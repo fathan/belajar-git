ba1800d66bcf2c856353f0d702769082bffcc631
fathan <kreasi.fathan@gmail.com>
2018-10-23 15:12:14 +0700

    updated index.js feature 2

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
-console.log('Hai, Fathan')
-console.log('Hai, saya dari feature 01')
+console.log("Hai, Fathan");
+console.log("Hai, saya dari feature 01");
+console.log("Hai, saya dari feature 02");
